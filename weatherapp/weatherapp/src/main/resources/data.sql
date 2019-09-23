b629b7571bf2b37bed01fa8dafd0f7b3e17b0cd6
sachinkolhe <sachukolhe@gmail.com>
2019-09-23 19:12:27 +0530

    deleted unnecessary files

--- weatherapp/weatherapp/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-/*DROP TABLE IF EXISTS weather;
- 
-CREATE TABLE weather (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  zipcode VARCHAR(250) NOT NULL,
-  local_date DATE NOT NULL,
-  local_time TIME DEFAULT NULL,
-  temparature TINYINT DEFAULT 0
-);*/
